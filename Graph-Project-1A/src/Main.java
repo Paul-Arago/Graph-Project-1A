d375c0c9b351211764087e181508549ad7dd28cc
Paul-Arago <paularago@outlook.fr>
2024-06-04 11:38:39 +0200

    Read user choice for who is bidding

--- Graph-Project-1A/src/Main.java
+++ Graph-Project-1A/src/Main.java
@@ -1,7 +1,19 @@
 import java.util.List;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        //User choice for who is bidding
+        Scanner scanner = new Scanner(System.in);
+        String biddingChoice = "";
+        while(!biddingChoice.equals("1") && !biddingChoice.equals("2")) {
+            System.out.print("Type 1 for school bidding or 2 for student bidding : ");
+            biddingChoice = scanner.nextLine();
+            if (!biddingChoice.equals("1") && !biddingChoice.equals("2")) {
+                System.out.print("Bad entry.\n");
+            }
+        }
+
         //Parsing CSV file
         try{
             ParserCSV parser = new ParserCSV();
